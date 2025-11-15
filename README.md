d727f4240465b0ab70e964a935ff7a5f136cd459
personalleandrocesar <leandrocesar.goo@gmail.com>
2025-11-15 18:24:12 -0300

    firt Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Decolonialidade
+
+## Tópicos à serem feitos:
+- [] Jogar os arquivos em HTML para Template/Nuxt4
---
+++ app/pages/index.vue
@@ -0,0 +1,301 @@
+<script setup>
+  import { mySwiper } from "https://unpkg.com/swiper/swiper-bundle.min.js";
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 15,
+        slidesPerGroup: 5,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    </script>
+<template>
+  <div>
+    <header>
+        <div class="header-top">
+                <div id="logo" class=" logo-icon">
+                    <a href="index.html"> 
+                        <img src="/img/logo-svg.svg" alt="">
+                    </a>
+                </div>
+                <div class="header-title">
+                    <p>Decolonialidade</p>
+                </div>
+                <div class="icon">
+                    <a href="Login.html"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fffbeeff" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg>
+                    </a>
+                </div>
+        </div>
+        <div class="header-down">
+            <img id="header-svg" src="/wavesNegative.svg" alt="">
+            <div class="header-down-cont">
+                <p> <a href="Gravações.html">Gravações</a></p>
+                <div class="vert-bar"></div>
+                <p> <a href="">Materiais</a></p>
+                <div class="vert-bar"></div>
+                <p><a href="">Artigos</a></p>
+            </div>
+        </div>
+    </header>
+
+
+    
+    <main>
+        <div class="main-hd"></div>
+
+        <div class="categorias-conteiner grid text-center">
+            
+            <div class="categ-box block-1">
+                <p>Artigos</p>
+                <div class="over-block-1"></div>
+                <div class="categoria-bg">
+                    <img src="/img/categoria-1.jpg" alt="">
+                </div>
+            </div>
+            <div class="categ-box block-2">
+                <p>Pedagógico</p>
+                <div class="over-block-2"></div>
+                <div class="categoria-bg">
+                    <img src="/img/categoria-2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="about-deco">
+            <div class="block-about"></div>
+            <div class="about-cont">
+                <div class="about-title">
+                    <p id="abt-1">SOBRE</p>
+                    <div class="abt-big-title">
+                        <p id="abt-2">Decolo</p>
+                        <p id="abt-3">nialidade</p>
+                    </div>
+                    
+                </div>
+                <div class="about-desc">
+                    O chamado pensamento decolonial, representa um conceito de formulações teóricas as quais vem sendo desenvolvidas desde o 
+                final do século XX, através do grupo de pesquisa: Modernidade/Colonialidade. Consuma-se que o termo <i>decolonial</i>
+                expressa a necessidade de produção de um pensamento sob o ponto de vista dos subalternizados pela Modernidade
+                capitalista politicamente em contraposição à perspectiva eurocêntrica hegemônica na academia em relação 
+                ao conhecimento histórico e social.
+                <br>
+                    <a href="" class="about-pag">
+    
+                        <p>Sabia mais</p> 
+                        <img src="/img/arrow.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="about-img">
+                <img src="https://www.pngall.com/wp-content/uploads/4/Website-PNG-Download-Image.png" alt="">
+            </div>
+        </div>
+
+
+
+
+        <section class="membros-deco swiper-container mySwiper">
+            <a href="" class="membro-title">
+                <p>Nossos Membros </p>
+                <div class="membro-title-svg">
+                    <svg viewBox="-4.5 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#2a2c24"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>arrow_right [#336]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-305.000000, -6679.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769" id="arrow_right-[#336]"> </path> </g> </g> </g> </g></svg>
+                </div>
+            </a>
+            
+            <div class="cards-deco swiper-wrapper">
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/ricardo.webp" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Ricardo Cesar Rocha da Costa
+                        </div>
+                        <div class="membro-desc">
+                            Licenciado e bacharel em Ciências Sociais pela Universidade Federal Fluminense (UFF). 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Fernando Brame.jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Fernando Brame
+                        </div>
+                        <div class="membro-desc">
+                            professor de Sociologia do IFRJ - campus Nilópolis. Mestre e doutor em Ciências Sociais.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/José Manuel Faria.jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            José Manuel Faria
+                        </div>
+                        <div class="membro-desc">
+                            Professor de História da Rede Estadual de Ensino (SEEDUC/RJ). Mestre em História Social pela UERJ/FFP. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Luiz Claudio (2).jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Luiz Claudio da Silva
+                        </div>
+                        <div class="membro-desc">
+                            Professor de matemática (aposentado) da Rede Estadual de Educação (SEEDUC/RJ).
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Ortelia Moraes.jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Maria Ortelia Moraes
+                        </div>
+                        <div class="membro-desc">
+                            Professora de História. Participa da Direção do SINPRO-Niterói, do GEPECD e do Coletivo Casulo.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Paulo Gomes (2).jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Paulo Gomes Coutinho
+                        </div>
+                        <div class="membro-desc">
+                            Licenciado e bacharel em História pela Faculdade de Humanidades Pedro II (1988). 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Roberto Carlos Costa.jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Roberto Carlos Costa da Silva
+                        </div>
+                        <div class="membro-desc">
+                            Licenciado em História pela Universidade Estácio de Sá. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/membros/Sérgio Oliveira.jpg" alt="">
+                    </div>
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            Sérgio Oliveira da Silva
+                        </div>
+                        <div class="membro-desc">
+                            Professor de Filosofia (aposentado) da Rede Estadual de Educação (SEEDUC/RJ), da Faetec e de escolas privadas. 
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="membro-card swiper-slide">
+                    <div class="membro-img">
+                        <img src="/img/ricardo.webp" alt="">
+                    </div>
+
+                    <div class="membro-cont">
+                        <div class="membro-nome h5">
+                            George Luiz de Abreu Vidipó
+                        </div>
+                        <div class="membro-desc">
+                            Licenciado em História pelo Centro Universitário Augusto Motta.
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        </section>
+
+
+
+   
+
+        <div class="main-fd"></div>
+    </main>
+    <footer>
+        <div class="footer-cont">
+            <div class="about-footer">
+                <div class="about-cont">
+                <p class="h4"> Mais Decolonialidade</p>
+                <hr>
+                 <a href=""><p>Sobre nós</p></a>
+                 <a href=""><p>Fale Conosco</p></a>
+                 <a href="Intregrantes.html"><p>Integrantes</p></a>
+                </div>
+                
+            </div>
+            <div class="vert-bar-footer"></div>
+            <div class="social-footer">
+                <div class="social-cont">
+                    <div class="social-cont-icon">
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-facebook" viewBox="0 0 16 16">
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                            </svg>
+                        </a>
+                        <a href="">Facebook</a>
+                    </div>
+
+                    <div class="social-cont-icon">
+                        <a href="">
+                             <svg id="mail" width="556px" height="556px" viewBox="0 0 192.00 192.00" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path stroke="#ffffff" stroke-linejoin="round" stroke-width="14.207999999999998" d="M22 57.265V142c0 5.523 4.477 10 10 10h24V95.056l40 30.278 40-30.278V152h24c5.523 0 10-4.477 10-10V57.265c0-13.233-15.15-20.746-25.684-12.736L96 81.265 47.684 44.53C37.15 36.519 22 44.032 22 57.265Z"></path></g></svg>
+                        </a>
+                        <a href="">Email</a>
+                    </div>
+
+                    <div class="social-cont-icon">
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-instagram" viewBox="0 0 16 16">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                            </svg>
+                        </a>
+                        <a href="">Instragram</a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </footer>
+  </div>
+</template>
